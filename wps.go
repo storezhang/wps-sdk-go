a8b63ebcbd1c91dcbf7cb58cd32615c623aa6d5d
storezhang <storezhang@gmail.com>
2020-09-28 19:17:27 +0800

    增加内部浏览地址

--- wps.go
+++ wps.go
@@ -16,11 +16,7 @@ const (
 // Wps 金山文档
 type Wps struct {
 	// 服务器地址
-	Host string `json:"ip"`
-	// 服务器端口
-	Port int `json:"port"`
-	// 协议，支持HTTP和HTTPS
-	Scheme string `default:"http" json:"scheme"`
+	Url string `json:"url"`
 	// 文档预览前缀
 	PreviewPrefix string `default:"web" json:"previewPrefix"`
 	// 内部文档预览前缀
@@ -36,13 +32,13 @@ func (w Wps) String() string {
 }
 
 func (w *Wps) previewUrl() string {
-	return fmt.Sprintf("%s://%s:%d/%s", w.Scheme, w.Host, w.Port, w.PreviewPrefix)
+	return fmt.Sprintf("%s/%s", w.Url, w.PreviewPrefix)
 }
 
 func (w *Wps) innerPreviewUrl() string {
-	return fmt.Sprintf("%s://%s:%d/%s", w.Scheme, w.Host, w.Port, w.InnerPreviewPrefix)
+	return fmt.Sprintf("%s/%s", w.Url, w.InnerPreviewPrefix)
 }
 
 func (w *Wps) convertUrl() string {
-	return fmt.Sprintf("%s://%s:%d/%s", w.Scheme, w.Host, w.Port, w.ConvertPrefix)
+	return fmt.Sprintf("%s/%s", w.Url, w.ConvertPrefix)
 }
